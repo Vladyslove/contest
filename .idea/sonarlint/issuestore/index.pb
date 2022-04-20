f116c7b5429bf02dd1a64d391e08f3abf8c841a5
vladyslovestepanov <vstepanov@neonomics.io>
2022-04-20 20:55:05 +0300

    added first Contest contract and migration file

---
+++ .idea/sonarlint/issuestore/index.pb
@@ -0,0 +1,3 @@
+
+E
+contracts/Contest.sol,7/b/7b556628a5d95e2d475d832c8c9a9454dab377c1
---
+++ contracts/Contest.sol
@@ -0,0 +1,15 @@
+// SPDX-License-Identifier: GPL-3.0
+pragma solidity >=0.4.22 <0.9.0;
+
+// creating the contract
+/**
+ * The Contest contract does this and that...
+ */
+contract Contest {
+
+    string public contestant;
+
+    constructor () public {
+        contestant = "Tom";
+    }
+}
