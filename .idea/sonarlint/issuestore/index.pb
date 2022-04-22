7cdb1a89d2ba4045a925b2ebb7f809997dd2f112
vladyslovestepanov <vstepanov@neonomics.io>
2022-04-23 02:10:23 +0300

    added test for voting functionality

--- .idea/sonarlint/issuestore/index.pb
+++
@@ -1,5 +0,0 @@
-
-=
-src/js/app.js,d/f/dfa7e20d89fdea717938db2bf088fdea8c2f0616
->
-src/index.html,5/c/5c975582791da6f060261188096d6aad85db6e09
--- test/contest.js
+++ test/contest.js
@@ -10,13 +10,15 @@ contract("Contest", function(accounts) {
         });
     });
 
+
+
     it("it initializes the contestants with the correct values", function() {
         return Contest.deployed().then(function(instance) {
             contestInstance = instance
             return contestInstance.contestants(1)
         }).then(function(contestant) {
             assert.equal(contestant[0], 1, "contains the correct id")
-            assert.equal(contestant[1], "Barbos", "contains the correct name")
+            assert.equal(contestant[1], "Tom", "contains the correct name")
             assert.equal(contestant[2], 0, "contains the correct votes count")
             return contestInstance.contestants(2)
         }).then(function(contestant) {
@@ -25,6 +27,28 @@ contract("Contest", function(accounts) {
             assert.equal(contestant[2], 0, "contains the correct votes count")
         })
     })
+
+
+
+    it("allows a voter to cast a vote", function() {
+        return Contest.deployed().then(function(instance) {
+            contestInstance = instance;
+            contestantId = 2;
+            return contestInstance.vote(contestantId, { from: accounts[0]});
+        }).then(function(receipt) {
+            return contestInstance.voters(accounts[0]);
+        }).then(function(voted) {
+            assert(voted, "the voter was marked as voted")
+            return contestInstance.contestants(contestantId);
+        }).then(function(contestant){
+            var voteCount = contestant[2];
+            assert.equal(voteCount, 1, "increments the contestant's vote count")
+        })
+    })
+
+
+
+
 
 
 });
